91ce0ad505b525fb9c951c03405d7fc331ddc4f4
JoseAGomezBravo <04josegomez2022@gmail.com>
2023-12-10 22:54:42 -0800

    commit javascript responses

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("Something that worked really well these last 9 weeks, were looking back at older assignments to help me reinforce my knowledge, and allow me to better use that along new concepts we are learning.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("Something that I would change from this course would be to increase and extend the amount of examples and walk through of new concepts we are learning");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("Yes I learned everything that I expected to learn from this course.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("A concept that was new to me were event listeners, concepts that I already encountered in the past were if, if else, else, and how to assign variables.");
